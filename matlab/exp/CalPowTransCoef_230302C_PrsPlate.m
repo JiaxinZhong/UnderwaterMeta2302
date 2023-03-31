d8fc233abaa8f646435412eaf26acfa4c48c19ef
psujunji <101603012+psujunji@users.noreply.github.com>
2023-03-31 11:26:04 -0400

    Add files via upload

--- matlab/exp/CalPowTransCoef_230302C_PrsPlate.m
+++ matlab/exp/CalPowTransCoef_230302C_PrsPlate.m
@@ -1,17 +1,23 @@
 clear all
 
 load('CalPowTransCoef_230302C_PrsPlate.mat');
-load('vik.mat')
+load('c_data_max.mat')
+
 fn = mfilename;
 
 fig = figure;
-pcolor(x_data, y_data, c_data);
-colormap(vik)
-shading interp
-% title(sprintf('Without AMM, prsesure, %d kHz', f_des/1e3));
-xlabel('x (mm)');
-ylabel('z (mm)');
-colorbar
-% clim([0, 3.8e-3]);
+pp = pcolor(x_data, y_data, c_data/c_data_max);
 
+xlabel('$x$ (mm)', 'Interpreter','latex');
+ylabel('$y$ (mm)', 'Interpreter','latex');
+
+clim([0, 1]);
+tit = title('Plate w/o AMM');
+
+cc = colorbar;
+cc.Label.String = 'Normalized digital siginal';
+
+
+AAA = PcolorSettingFunc(pp,tit,cc)
+%%
 print(sprintf('exp/fig/%s.jpg', fn),'-djpeg', sprintf('-r%s',num2str(300)));
--- matlab/exp/CalPowTransCoef_230302C_PrsVoid.m
+++ matlab/exp/CalPowTransCoef_230302C_PrsVoid.m
@@ -1,16 +1,25 @@
 % clear all
 
 load('CalPowTransCoef_230302C_PrsVoid.mat');
-load('vik.mat')
 fn = mfilename;
 
+c_data_max = max(max(c_data));
+save exp\data\c_data_max.mat c_data_max 
+%%
+
 fig = figure;
-pcolor(x_data, y_data, c_data);
-colormap(vik)
-shading interp
-xlabel('x (mm)');
-ylabel('z (mm)');
-colorbar
-% clim([0, 3.8e-3]);
+pp = pcolor(x_data, y_data, c_data/c_data_max);
+
+xlabel('$x$ (mm)', 'Interpreter','latex');
+ylabel('$y$ (mm)', 'Interpreter','latex');
+
+clim([0, 1]);
+tit = title('Nothing');
+
+cc = colorbar;
+cc.Label.String = 'Normalized digital siginal';
+
+
+AAA = PcolorSettingFunc(pp,tit,cc)
 
 print(sprintf('exp/fig/%s.jpg', fn),'-djpeg', sprintf('-r%s',num2str(300)));
